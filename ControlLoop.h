29c13b09a1e31c669f56d9b30ad86a126d907d30
bolke <debolke@gmail.com>
2018-04-06 09:31:31 +0200

    refactoring and moving stuff to modbusmapping

--- ControlLoop.h
+++ ControlLoop.h
@@ -1,22 +1,21 @@
-#ifndef CONTROLLOOP_BLIB_H
-#define CONTROLLOOP_BLIB_H
+#ifndef CONTROLLOOP_H
+#define CONTROLLOOP_H
 
   #include <stdint.h>
-  #include "Ascada.h"
-  
-  #define ALARM_WORD_CNT 0x10
-  #define ALARM_BIT_CNT (ALARM_WORD_CNT*16)
+  #include "Tools.h"
+	#include "ModbusMapping.h"
 
-  #define CL_SETTING_SIZE 1
-  
   #define HALTED_NONE 0
   #define HALTED_HALTED 1
   #define HALTED_PAUSED 2
   #define HALTED_STOPPED 3
   
-  typedef struct {
-    union{
-      struct{
+  typedef struct
+	{
+    union
+		{
+      struct
+			{
         uint16_t isRunning:1;
         uint16_t halted:2;
         uint16_t unexpectedShutdown:3;
@@ -24,11 +23,12 @@
         uint16_t configLoaded:1;
       };
       uint16_t status = 0;              
-    };
-    uint16_t alarms[ALARM_WORD_CNT] = {0};      
-    union{
-      uint8_t settings[CL_SETTING_SIZE]={0};
-      struct{
+    };   
+    union
+		{
+      uint8_t settings[1]={0};
+      struct
+			{
         uint8_t startRunning:1;
         uint8_t defaultOffline:1;
       };
@@ -36,16 +36,11 @@
   } cl_t;
 
   extern cl_t cl_ds;
-
+  
   uint8_t clSetup();
   uint8_t clLoop();  
   uint8_t clStart();
   uint8_t clStop();
   uint8_t clPause();
-  
-  ReadFuncPtr clReadReg(uint16_t address,uint16_t* value);
-  ReadFuncPtr clReadBit(uint16_t address,uint16_t* value);
-  WriteFuncPtr clWriteReg(uint16_t address,uint16_t value);
-  WriteFuncPtr clWriteBit(uint16_t address,uint16_t value);
+	
 #endif
-
