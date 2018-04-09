5bbd6be744104ab7323407ca8970795fa359b3f8
bolke <debolke@gmail.com>
2018-04-09 10:02:41 +0200

    cleanup, moving all code to control loop

--- Ascada.ino
+++ Ascada.ino
@@ -5,28 +5,9 @@
 //--------------------------------------------------------------------------------------
 #include "Tools.h"
 #include "ControlLoop.h"
-#include "Modbus.h"
-//--------------------------------------------------------------------------------------
-void serialEvent() {
-	mbSerialEvent();
-}
 //--------------------------------------------------------------------------------------
 void setup()
-{
-  if (CheckEepromForMagic())
-  {
-    if (ConfigFromEeprom(0, MB_SETTING_SIZE, mb_ds.settings))
-    {
-      ConfigFromEeprom(MB_SETTING_SIZE, sizeof(mb_ds.settings), cl_ds.settings);
-      cl_ds.configLoaded=true;
-    }
-  }
-
-  mb_ds.ReadRegister=HandleModbusCall;
-  mb_ds.ReadBit=HandleModbusCall;
-  mb_ds.WriteRegister=HandleModbusCall;
-  mb_ds.WriteBit=HandleModbusCall;
-  
+{ 
   clSetup();
 }
 //--------------------------------------------------------------------------------------
