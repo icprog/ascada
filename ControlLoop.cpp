97d3b57fd451db91b7b16694a7afb6363efc68e9
bolke <debolke@gmail.com>
2018-04-09 10:12:14 +0200

    expanded initialisation, eeprom reading, modbus init, etc. implemented specific functions calle through modbus calls, reading values, writing values, executing other functions.

--- ControlLoop.cpp
+++ ControlLoop.cpp
@@ -9,7 +9,22 @@ cl_t cl_ds;
 //--------------------------------------------------------------------------------------
 uint8_t clSetup()
 {
+  if (CheckEepromForMagic())
+  {
+    if (ConfigFromEeprom(0, MB_SETTING_SIZE, mb_ds.settings))
+    {
+      ConfigFromEeprom(MB_SETTING_SIZE, sizeof(mb_ds.settings), cl_ds.settings);
+      cl_ds.configLoaded=true;
+    }
+  }
+  
   cl_ds.unexpectedShutdown=CheckResetRegister();
+
+  mb_ds.ReadRegister=HandleModbusRead;
+  mb_ds.ReadBit=HandleModbusRead;
+  mb_ds.WriteRegister=HandleModbusWrite;
+  mb_ds.WriteBit=HandleModbusWrite;
+  
 	if (cl_ds.configLoaded)
   {
     if ((!cl_ds.startRunning)&&cl_ds.defaultOffline)
@@ -20,9 +35,7 @@ uint8_t clSetup()
       clStart();
   }
   else
-  {
-    mbSetup(DEFAULT_BAUDRATE, DEFAULT_SLAVE_ID);
-  }
+    mbSetup(DEFAULT_BAUDRATE, DEFAULT_SLAVE_ID);  
 
   return EXCEPTION_NONE;
 }
@@ -77,64 +90,140 @@ uint8_t clPause()
 //--------------------------------------------------------------------------------------
 uint8_t ReadDeviceReg(uint16_t address,uint16_t* value)
 {  
-  *value=1;
+  *value=GET_REGISTER(address)&0xFF;
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t WriteDeviceReg(uint16_t address,uint16_t* value)
 {  
-  *value=2;
+  SET_REGISTER(address,*value);
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t ReadEepromReg(uint16_t address,uint16_t* value)
 {  
-  *value=3;
+  *value=EEPROM.read(address)&0xFF;
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t WriteEepromReg(uint16_t address,uint16_t* value)
 {  
-  *value=4;
+  EEPROM.write(address,*value);
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t ReadVersionReg(uint16_t address,uint16_t* value)
 {  
-  *value=5;
+  switch(address)
+  {
+    case 0:                      
+      *value=(VDATE[0]<<8)|(VDATE[1]);          
+      break;                                    
+    case 1:                    
+      *value=(VDATE[2]<<8)|(VDATE[4]);          
+      break;                                    
+    case 2:                    
+      *value=(VDATE[5]<<8)|(VDATE[7]);          
+      break;                                    
+    case 3:                    
+      *value=(VDATE[8]<<8)|(VDATE[9]);          
+      break;                                    
+    case 4:                    
+      *value=(VDATE[10]<<8)|(' ');              
+      break;                                    
+    case 5:                    
+      *value=(VTIME[0]<<8)|(VTIME[1]);          
+      break;                                    
+    case 6:                    
+      *value=(VTIME[3]<<8)|(VTIME[4]);          
+      break;                                    
+    case 7:                    
+      *value=(VTIME[6]<<8)|(VTIME[7]);          
+      break;            
+    default:
+      return EXCEPTION_INVALID_ADDRESS;
+  }                        
+  
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t ReadStatusReg(uint16_t address,uint16_t* value)
 {  
-  *value=6;
+  *value=cl_ds.status;
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t ReadSettingsReg(uint16_t address,uint16_t* value)
 {  
-  *value=7;
+  *value=cl_ds.settings[address];
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
 uint8_t ReadUptimeReg(uint16_t address,uint16_t* value)
-{  
-  *value=8;
+{      
+  *value=(millis()>>(address*16))&0xFFFF;  
   return EXCEPTION_NONE;
 }
 //--------------------------------------------------------------------------------------
-uint8_t HandleModbusCall(uint16_t address, uint16_t* value)
+uint8_t WriteFuncCoil(uint16_t address,uint16_t* value)
+{
+  uint8_t result=EXCEPTION_INVALID_ADDRESS;
+  if(*value==0xFF00){
+    switch (address)
+    {
+      case 0x0000:
+        result=clStart();
+        if ((result==EXCEPTION_NONE)&&cl_ds.defaultOffline)
+        {
+          if (ConfigFromEeprom(0, MB_SETTING_SIZE, mb_ds.settings))
+            mbSetup(mb_ds.baudrate, mb_ds.slaveId);
+        }
+        break;
+      case 0x0001:
+        result=clPause();
+        break;
+      case 0x0002:
+        result=clStop();
+        break;
+    }
+  }else
+    result=EXCEPTION_INVALID_VALUE;
+  return result;
+}
+//--------------------------------------------------------------------------------------
+uint8_t HandleModbusRead(uint16_t address, uint16_t* value)
 {  
+  modbusMapping_t target;
   for(uint16_t i=0;i<MB_CNT;i++)
   {
-    if(mbMapping[i].regStart<=address)
+    PROGRAM_READTYPE (&mbMapping [i], target);
+    if(target.isRead && target.regStart<=address)
     {
-      return mbMapping[i].regStart & 0xFF;
-      if((mbMapping[i].regStart+mbMapping[i].regCnt)>address)
-        return (*(mbMapping[i].funcPtr))(address-mbMapping[i].regStart,value);        
+      if(address<(target.regStart+target.regCnt)){      
+        return (*(target.funcPtr))(address-target.regStart,value);    
+      }    
     }
   }
   return EXCEPTION_INVALID_ADDRESS;
 }
 //--------------------------------------------------------------------------------------
-  
+uint8_t HandleModbusWrite(uint16_t address, uint16_t* value)
+{  
+  modbusMapping_t target;
+  for(uint16_t i=0;i<MB_CNT;i++)
+  {
+    PROGRAM_READTYPE (&mbMapping [i], target);
+    if(!target.isRead && target.regStart<=address)
+    {
+      if(address<(target.regStart+target.regCnt)){      
+        return (*(target.funcPtr))(address-target.regStart,value);    
+      }    
+    }
+  }
+  return EXCEPTION_INVALID_ADDRESS;
+}
+//--------------------------------------------------------------------------------------
+void serialEvent() 
+{
+  mbSerialEvent();
+}
